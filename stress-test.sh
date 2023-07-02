7f27dca0d0bd7c5a08473e92d7cfdcea8eb51c51
mavlyut <ervin.mavltyutov@mail.ru>
2023-07-02 05:07:11 +0300

    script fixed

--- stress-test.sh
+++ stress-test.sh
@@ -12,11 +12,22 @@ testdir="tests"
 failed="failed_test_input"
 jarfile="stress-test.jar"
 manifest="MANIFEST.MF"
+compiledir="build"
 testcnt=10
+trash="trash"
+
+function search_good_compile_dir() {
+  i=0
+  while [ -d $compiledir$i ]; do
+    i=$(($i + 1))
+  done
+  compiledir=$compiledir$i
+  mkdir $compiledir
+}
 
 function print_usage() {
   echo "$1"
-  echo "Usage: <input-config> <good-solution> <bad-solution>"
+  echo "Usage: <input-config> <good-solution> <bad-solution> [<tests-in-group>]"
   exit 20
 }
 
@@ -35,9 +46,9 @@ function check_existing() {
 
 function compile() {
   if [[ $1 =~ ".java" ]]; then
-    javac $1
+    javac $1 -d $compiledir
   elif [[ $1 =~ ".cpp" ]]; then
-    g++ -o moscow.out $1
+    g++ -o $compiledir/$1.out $1
   elif [[ $1 =~ ".kt" ]]; then
     kotlinc $1
   else
@@ -50,7 +61,7 @@ function run() {
     classname=${1::-5}
     java $classname
   elif [[ $1 =~ ".cpp" ]]; then
-    ./moscow.out
+    ./$1.out
   elif [[ $1 =~ ".kt" ]]; then
     classname=${1::-3}
     kotlin $$classname
@@ -59,25 +70,35 @@ function run() {
   fi < $2 > $3
 }
 
-function clean() {
-  if [[ $1 =~ ".java" ]]; then
-    classname=${1::-5}
-    rm $classname.class
-  elif [[ $1 =~ ".cpp" ]]; then
-    rm ./moscow.out
-  elif [[ $1 =~ ".kt" ]]; then
-     classname=${1::-3}
-     rm $classname.class
-  else
-    echo "Расширение не поддерживается"
-  fi
+# fix?
+function get_truth_name() {
+  echo $1 | rev | cut -d '/' -f 1 | rev
+}
+
+function finish() {
+  {
+    cd ..
+    rm -f $out1 $out2 $manifest $jarfile && removedir $testdir && removedir $compiledir && echo "[+] Files deleted!"
+  } || {
+    echo "Error while deleting files"
+    exit 84
+  }
+}
+
+function error() {
+  echo $1
+  finish
+  exit $2
 }
 
 
-if [[ $# -ne 3 ]]; then
-  print_usage "Count of args must be equals 3"
+if [[ $# -ne 3 ]] && [[ $# -ne 4 ]]; then
+  print_usage "Illegal count of arguments"
+elif [[ $# -eq 4 ]]; then
+  testcnt=$4
 fi
 
+search_good_compile_dir
 check_existing $1 Config
 check_existing $2 Good-solution
 check_existing $3 Bad-solution
@@ -87,45 +108,48 @@ echo "Main-Class: parser.ConfigureParser" >> $manifest
 
 # make jar
 {
-  javac -d OUT $(find . -name \*.java) &&
-    jar --create --manifest $manifest --file $jarfile -C OUT . &&
-    removedir OUT
+  javac -d OUT $(find src -name \*.java) &&
+    jar --create --manifest $manifest --file $compiledir/$jarfile -C OUT . &&
+    removedir OUT && echo "[+] Jar-file created!"
 } || {
-  echo "Error while making jar-file"
-  exit 95
+  error 115 "Error while making jar-file"
 }
 
 # compile
 {
-  compile $2 && compile $3 && echo "Build completed!"
+  compile $2 && compile $3 && echo "[+] Build completed!"
+  touch $compiledir/$trash
 } || {
-  echo "Error while compiling"
-  exit 103
+  error 123 "Error while compiling"
 }
 
 # run tests
+cd $compiledir
 fl="1"
 for (( c = 1; c <= 10; c+=1 )) do
-  java -jar stress-test.jar $1 $testcnt $testdir
-  for (( i = 1; i <= $((testcnt)); i++ )); do
+  {
+    java -jar stress-test.jar ../$1 $testcnt $testdir
+  } || {
+    error 134 "Error while generating tests"
+  }
+  for (( i = 1; i <= $testcnt; i++ )); do
     {
-      run $2 $testdir/test$i $out1
+      run $2 $testdir/test$i $out1 > $trash
     } || {
-      i=$(($i - 1))
       continue
     }
     {
-      run $3 $testdir/test$i $out2
+      run $3 $testdir/test$i $out2 > $trash
     } || {
-      echo "Test $(( ($c - 1) * $testcnt + $i )) failed!"
+      echo "[-] Test $(( ($c - 1) * $testcnt + $i )) failed!"
       echo "Testcase - in file \"$failed\""
-      cp $testdir/test$i $failed
+      cp $testdir/test$i ../$failed
       echo Expected: $(cat $out1), found: exception
       fl="0"
       break
     }
     if [[ $(diff $out1 $out2) ]]; then
-      echo "Test $(( ($c - 1) * $testcnt + $i )) failed!"
+      echo "[-] Test $(( ($c - 1) * $testcnt + $i )) failed!"
       echo "Testcase - in file \"$failed\""
       cp $testdir/test$i $failed
       echo Expected: $(cat $out1), found: $(cat $out2)
@@ -134,13 +158,10 @@ for (( c = 1; c <= 10; c+=1 )) do
     fi
   done
   if [[ $fl == "0" ]]; then break; fi
-  echo $(($testcnt * $c)) tests passed!
+  echo "[+]" $(($testcnt * $c)) "tests passed!"
 done
 
+exit 158
+
 # cleaning
-{
-  rm -f $out1 $out2 $manifest $jarfile && removedir $testdir && clean $2 && clean $3
-} || {
-  echo "Error while deleting files"
-  exit 134
-}
+finish
